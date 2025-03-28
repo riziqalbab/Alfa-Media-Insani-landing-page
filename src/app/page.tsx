c7abd8ce921ae78119fe3796bcc4721ea2c8ed7c
riziqalbab <albabriziq36@gmail.com>
2025-03-28 08:16:14 +0700

    refactor: rename Home component to Page for clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar";
 import Testimoni from "@/components/Testimoni";
 import Footer from "@/components/Footer";
 
-export default function Home() {
+export default function Page() {
   return (
     <>
       <Navbar />
