d954ce47f686088ae38ed40c4250c04f97b580d0
riziqalbab <albabriziq36@gmail.com>
2025-03-28 08:25:04 +0700

    m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,17 @@
-import { Suspense } from 'react';
+// import About from "@/components/About";
 import BookCatalog from "@/components/BookCatalog";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Testimoni from "@/components/Testimoni";
 import Footer from "@/components/Footer";
 
-export default function Page() {
+export default function Home() {
   return (
     <>
       <Navbar />
       <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-        <BookCatalog />
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Testimoni />
-      </Suspense>
+      <BookCatalog />
+      <Testimoni />
       <Footer />
     </>
   );
